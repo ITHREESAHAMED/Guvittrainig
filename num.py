9abdd64f0208e8019190bc5635b5edc6fea1ac09
MOHAMED ITHREES AHAMED <37235093+ITHREESAHAMED@users.noreply.github.com>
2018-03-10 12:54:07 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,11 @@
+
+x=int(input("enter only number"))
+if x<0:
+    print('NEGATIVE')
+elif x==0:
+    print('ZERO')
+elif x>100000:
+    print('enter value below 100000')
+else:
+    print('POSITIVE')
+
